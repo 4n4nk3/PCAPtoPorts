2e4d5aae98a6a2f57a5907f4bb385337ed5983c5
4n4nk3 <47717886+4n4nk3@users.noreply.github.com>
2020-04-03 13:27:35 +0200

    Added parameters: tcp, udp, srcport, dstport. Rewrote code to output results as a function. Cleaned up code and added a check for pcap file existence.

--- PCAPtoPorts.py
+++ PCAPtoPorts.py
@@ -4,103 +4,174 @@
 
 # Written By Ananke: https://github.com/4n4nk3
 import argparse
-from colorama import init, Back, Style
+from sys import exit
+from os import path
+from collections import defaultdict
+
 import pyshark
+from colorama import init, Back, Fore, Style
 
 init()
 
-parser = argparse.ArgumentParser(
-    description='A simple parser written in python to quickly get ports and connections related to a specific HOST from a PCAP file.')
-required = parser.add_argument_group('required arguments')
-# Required arguments
-required.add_argument('--input', help='Input file name', required=True)
-required.add_argument('--host', help='Specify the IP address of the host on which you want to investigate',
-                      required=True)
+incoming = defaultdict(set)
+outgoing = defaultdict(set)
 
-args = parser.parse_args()
-incoming = {}
-outgoing = {}
+def init_argparse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description='A simple parser written in python to quickly get ports and connections related to a specific HOST '
+                    'from a PCAP file.')
+    # Required arguments
+    parser.add_argument('--input', help='Input file name', required=True)
+    parser.add_argument('--host', help='Specify the IP address of the host on which you want to investigate',
+                          required=True)
+    # Optional arguments
+    parser.add_argument('--tcp', help='Show TCP connections', required=False, action='store_true')
+    parser.add_argument('--udp', help='Show UDP connections', required=False, action='store_true')
+    parser.add_argument('--srcport', help='Display source ports for shown connections', required=False, action='store_true')
+    parser.add_argument('--dstport', help='Display destination ports for shown connections', required=False, action='store_true')
+    return parser
 
-try:
-    # Open pcap file
-    cap = pyshark.FileCapture(args.input)
-    for packet in cap:
-        if 'IP' in packet:
-            destination_ip = str(packet.ip.dst)
-            origin_ip = str(packet.ip.src)
-            # Check if target IP is involved with the current packet
-            if destination_ip == args.host or origin_ip == args.host:
-                # Check if current packet is TCP or UDP and so if I can get PORT's data from it
-                if 'UDP' in packet or 'TCP' in packet:
-                    if 'UDP' in packet:
-                        packet_type = 'udp'
-                        destination_port = str(packet.udp.dstport)
-                        source_port = str(packet.udp.port)
-                    else:
-                        packet_type = 'tcp'
-                        destination_port = str(packet.tcp.dstport)
-                        source_port = str(packet.tcp.port)
-                    # If the destination IP is the same as the target IP the packet is INCOMING to the target
-                    if destination_ip == args.host:
-                        if origin_ip not in incoming:
-                            incoming[origin_ip] = set()
-                        else:
-                            incoming[origin_ip].add(
-                                packet_type + ':' + source_port + ':' + destination_port + ':' + origin_ip)
-                    # If the origin IP is the same as the target IP the packet is OUTGOING from the target
-                    elif origin_ip == args.host:
-                        if destination_ip not in outgoing:
-                            outgoing[destination_ip] = set()
-                        else:
-                            outgoing[destination_ip].add(
-                                packet_type + ':' + source_port + ':' + destination_port + ':' + destination_ip)
 
-    # Print the results
-    output_header = ['TYPE', 'SOURCE PORT', 'DESTINATION PORT', 'ORIGIN IP']
-    print('\nINCOMING TRAFFIC')
-    for ip in incoming:
-        check = False
-        for connection in incoming[ip]:
-            output = connection.split(':')
-            if len(output) == 4:
-                check = True
-                break
-        if check is True:
-            print(Style.DIM + '\t' + '=' * 100 + Style.RESET_ALL)
-            print('\t{}Incoming{} traffic from IP:\t\t\t\t\t\t\t{}{}{}'.format(Back.LIGHTBLUE_EX, Style.RESET_ALL,
-                                                                               Back.LIGHTBLUE_EX, ip, Style.RESET_ALL))
-            print(Style.DIM + '\t' + '-' * 100 + Style.RESET_ALL)
-            print('\t' + ''.join(element.ljust(25) for element in output_header))
-            print(Style.DIM + '\t' + '-' * 100 + Style.RESET_ALL)
-            for connection in incoming[ip]:
-                print('\t' + ''.join(element.ljust(25) for element in output))
-            print('\n\n')
-
-    output_header = ['TYPE', 'SOURCE PORT', 'DESTINATION PORT', 'DESTINATION IP']
-    print('\n' + Back.LIGHTRED_EX + ' ' * 108 + Style.RESET_ALL)
-    print('OUTGOING TRAFFIC')
-    for ip in outgoing:
-        check = False
-        for connection in outgoing[ip]:
-            output = connection.split(':')
-            if len(output) == 4:
-                check = True
-                break
-        if check is True:
-            print(Style.DIM + '\t' + '=' * 100 + Style.RESET_ALL)
-            print('\t{}Outgoing{} traffic to IP:\t\t\t\t\t\t\t\t{}{}{}'.format(Back.LIGHTBLUE_EX, Style.RESET_ALL,
-                                                                               Back.LIGHTBLUE_EX, ip, Style.RESET_ALL))
-            print(Style.DIM + '\t' + '-' * 100 + Style.RESET_ALL)
-            print('\t' + ''.join(element.ljust(25) for element in output_header))
-            print(Style.DIM + '\t' + '-' * 100 + Style.RESET_ALL)
-            for connection in outgoing[ip]:
-                output = connection.split(':')
-                print('\t' + ''.join(element.ljust(25) for element in output))
-            print('\n\n')
-
-except Exception as exception:
-    if isinstance(exception, FileNotFoundError):
-        print('Input file not found!\nExiting...')
+def result_printer(indicator, data):
+    if indicator == 'incoming':
+        if args.srcport is True and args.dstport is True:
+            output_header = ['TYPE', 'SOURCE PORT', 'DESTINATION PORT', 'ORIGIN IP']
+        elif args.srcport is True:
+            output_header = ['TYPE', 'SOURCE PORT', 'ORIGIN IP']
+        elif args.dstport is True:
+            output_header = ['TYPE', 'DESTINATION PORT', 'ORIGIN IP']
+        else:
+            output_header = ['TYPE', 'ORIGIN IP']
+        print('\nINCOMING TRAFFIC')
+    elif indicator == 'outgoing':
+        if args.srcport is True and args.dstport is True:
+            output_header = ['TYPE', 'SOURCE PORT', 'DESTINATION PORT', 'DESTINATION IP']
+        elif args.srcport is True:
+            output_header = ['TYPE', 'SOURCE PORT', 'DESTINATION IP']
+        elif args.dstport is True:
+            output_header = ['TYPE', 'DESTINATION PORT', 'DESTINATION IP']
+        else:
+            output_header = ['TYPE', 'DESTINATION IP']
+        print('\nOUTGOING TRAFFIC')
     else:
-        print(exception)
+        return
+    for ip in data:
+        print()
+        print(Style.DIM + '\t' + '=' * 100 + Style.RESET_ALL)
+        if indicator == 'incoming':
+            print(f'\t{Fore.LIGHTRED_EX}Incoming{Style.RESET_ALL} traffic from IP:\t\t\t\t\t\t\t{Fore.LIGHTGREEN_EX}{ip}{Style.RESET_ALL}')
+        else:
+            print(f'\t{Fore.LIGHTRED_EX}Outgoing{Style.RESET_ALL} traffic to IP:\t\t\t\t\t\t\t\t{Fore.LIGHTGREEN_EX}{ip}{Style.RESET_ALL}')
+        print(Style.DIM + '\t' + '-' * 100 + Style.RESET_ALL)
+        print('\t' + ''.join(element.ljust(25) for element in output_header))
+        print(Style.DIM + '\t' + '-' * 100 + Style.RESET_ALL)
+        for connection in data[ip]:
+            output = connection.split(':')
+            print('\t' + ''.join(element.ljust(25) for element in output))
+        #print('\n\n')
+
+
+
+
+parser = init_argparse()
+args = parser.parse_args()
+
+if args.tcp is False and args.udp is False:
+    parser.error('In order to run PCAPtoPorts you need to specify at least one protocol!')
+
+if path.isfile(args.input) is False:
+    print('Input file not found!\nExiting...')
     exit(1)
+
+# Open pcap file
+cap = pyshark.FileCapture(args.input)
+for packet in cap:
+    if 'IP' in packet:
+        destination_ip = str(packet.ip.dst)
+        origin_ip = str(packet.ip.src)
+        # Check if target IP is involved with the current packet
+        if destination_ip == args.host or origin_ip == args.host:
+            # Check if current packet is TCP or UDP and so if I can get PORT's data from it
+            if 'UDP' in packet and args.udp is True:
+                packet_type = 'udp'
+                destination_port = str(packet.udp.dstport)
+                source_port = str(packet.udp.port)
+            elif 'TCP' in packet and args.tcp is True:
+                packet_type = 'tcp'
+                destination_port = str(packet.tcp.dstport)
+                source_port = str(packet.tcp.port)
+            else:
+                continue
+            # If the destination IP is the same as the target IP the packet is INCOMING to the target
+            if destination_ip == args.host:
+                if args.srcport is True and args.dstport is True:
+                    incoming[origin_ip].add(packet_type + ':' + source_port + ':' + destination_port + ':' + origin_ip)
+                elif args.srcport is True:
+                    incoming[origin_ip].add(packet_type + ':' + source_port + ':' + origin_ip)
+                elif args.dstport is True:
+                    incoming[origin_ip].add(packet_type + ':' + destination_port + ':' + origin_ip)
+                else:
+                    incoming[origin_ip].add(packet_type + ':' + origin_ip)
+            # If the origin IP is the same as the target IP the packet is OUTGOING from the target
+            elif origin_ip == args.host:
+                if args.srcport is True and args.dstport is True:
+                    outgoing[destination_ip].add(
+                        packet_type + ':' + source_port + ':' + destination_port + ':' + destination_ip)
+                elif args.srcport is True:
+                    outgoing[destination_ip].add(packet_type + ':' + source_port + ':' + destination_ip)
+                elif args.dstport is True:
+                    outgoing[destination_ip].add(packet_type + ':' + destination_port + ':' + destination_ip)
+                else:
+                    outgoing[destination_ip].add(packet_type + ':' + destination_ip)
+
+result_printer('incoming', incoming)
+print('\n' + Back.LIGHTBLUE_EX + ' ' * 108 + Style.RESET_ALL)
+result_printer('outgoing', outgoing)
+
+
+"""
+# Print the results
+if args.srcport is True and args.dstport is True:
+    output_header = ['TYPE', 'SOURCE PORT', 'DESTINATION PORT', 'ORIGIN IP']
+elif args.srcport is True:
+    output_header = ['TYPE', 'SOURCE PORT', 'ORIGIN IP']
+elif args.dstport is True:
+    output_header = ['TYPE', 'DESTINATION PORT', 'ORIGIN IP']
+else:
+    output_header = ['TYPE', 'ORIGIN IP']
+
+print('\nINCOMING TRAFFIC')
+for ip in incoming:
+    print(Style.DIM + '\t' + '=' * 100 + Style.RESET_ALL)
+    print('\t{}Incoming{} traffic from IP:\t\t\t\t\t\t\t{}{}{}'.format(Back.LIGHTBLUE_EX, Style.RESET_ALL,
+                                                                       Back.LIGHTBLUE_EX, ip, Style.RESET_ALL))
+    print(Style.DIM + '\t' + '-' * 100 + Style.RESET_ALL)
+    print('\t' + ''.join(element.ljust(25) for element in output_header))
+    print(Style.DIM + '\t' + '-' * 100 + Style.RESET_ALL)
+    for connection in incoming[ip]:
+        output = connection.split(':')
+        print('\t' + ''.join(element.ljust(25) for element in output))
+    print('\n\n')
+
+if args.srcport is True and args.dstport is True:
+    output_header = ['TYPE', 'SOURCE PORT', 'DESTINATION PORT', 'DESTINATION IP']
+elif args.srcport is True:
+    output_header = ['TYPE', 'SOURCE PORT', 'DESTINATION IP']
+elif args.dstport is True:
+    output_header = ['TYPE', 'DESTINATION PORT', 'DESTINATION IP']
+else:
+    output_header = ['TYPE', 'DESTINATION IP']
+
+print('\n' + Back.LIGHTRED_EX + ' ' * 108 + Style.RESET_ALL)
+print('OUTGOING TRAFFIC')
+for ip in outgoing:
+    print(Style.DIM + '\t' + '=' * 100 + Style.RESET_ALL)
+    print('\t{}Outgoing{} traffic to IP:\t\t\t\t\t\t\t\t{}{}{}'.format(Back.LIGHTBLUE_EX, Style.RESET_ALL,
+                                                                       Back.LIGHTBLUE_EX, ip, Style.RESET_ALL))
+    print(Style.DIM + '\t' + '-' * 100 + Style.RESET_ALL)
+    print('\t' + ''.join(element.ljust(25) for element in output_header))
+    print(Style.DIM + '\t' + '-' * 100 + Style.RESET_ALL)
+    for connection in outgoing[ip]:
+        output = connection.split(':')
+        print('\t' + ''.join(element.ljust(25) for element in output))
+    print('\n\n')"""
